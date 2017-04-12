a62af47c6d0c96231176bec6a83d306399929d3d
Gavin Olsen <ols15015@byui.edu>
2017-04-12 10:31:22 -0600

    cleaning up the project

---
+++ StudentPut/StudentTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StudentTableViewController.swift
+//  StudentPut
+//
+//  Created by Gavin Olsen on 4/12/17.
+//  Copyright © 2017 Gavin Olsen. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+  
+}
